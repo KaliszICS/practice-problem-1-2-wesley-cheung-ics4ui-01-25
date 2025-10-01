22527cf4c60bdb177bbfa6357a35e5fc990931f8
wesley-cheung-ics4ui-01-25 <166552362+wesley-cheung-ics4ui-01-25@users.noreply.github.com>
2025-10-01 17:36:21 +0000

    update practiceproblem

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,6 +3,33 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static boolean validIndex(int[] input, int index) {
+		try {
+			if((input[index])!=132323233) {
+				return true;
+			}
+			return true;
+		}
+		catch (ArrayIndexOutOfBoundsException aIE) {
+			return false;
+		}
+	}
+	public static int divide(int one, int two) {
+		try {
+			return one/two;
+		}
+		catch (ArithmeticException aE) {
+			return 0;
+		}
+	}
+	public static int safeConvertStringtoInt(String input) {
+		try {
+			return Integer.parseInt(input);
+		}
+		catch (NumberFormatException nFE) {
+			return 0;
+		}
+	}
 
 	
 
